83059af8d06b868b514bf71ac35d4aeaa58544a6
HallexCosta <hallex.costa@hotmail.com>
2021-09-26 17:31:15 -0300

    refactor: convert plug.vim to plug.lua

---
+++ plug.lua
@@ -0,0 +1,33 @@
+local fn = vim.fn
+local call = vim.call
+
+local Plug = fn['plug#']
+local stdpath = fn.stdpath
+local has = fn.has
+
+local plug_home
+
+if has("nvim") then
+  plug_home = stdpath('data') .. '/plugged'
+end
+
+call('plug#begin', plug_home)
+
+Plug 'tpope/vim-fugitive'
+Plug 'tpope/vim-rhubarb'
+
+Plug 'cohama/lexima.vim'
+
+if has("nvim") then
+  Plug('neovim/nvim-lspconfig')
+  Plug('glepnir/lspsaga.nvim')
+  Plug('nvim-treesitter/nvim-treesitter', {['do'] = ':TSUpdate'})
+  Plug('nvim-lua/completion-nvim')
+  Plug('nvim-lua/popup.nvim')
+  Plug('nvim-lua/plenary.nvim')
+  Plug('nvim-telescope/telescope.nvim')
+  Plug('kyazdani42/nvim-web-devicons')
+  Plug('hoob3rt/lualine.nvim')
+end
+
+call('plug#end')
